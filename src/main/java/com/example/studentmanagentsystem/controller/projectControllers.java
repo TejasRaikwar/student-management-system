a293d205bec228c09c03d428a5dafb7beb203bc5
Tejas Raikwar <raikwar.tejas@gmail.com>
2023-10-17 09:02:38 +0530

    -

--- src/main/java/com/example/studentmanagentsystem/controller/projectControllers.java
+++ src/main/java/com/example/studentmanagentsystem/controller/projectControllers.java
@@ -1,5 +1,7 @@
 package com.example.studentmanagentsystem.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Controller;
@@ -8,9 +10,13 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.studentmanagentsystem.dto.CourseDAO;
+import com.example.studentmanagentsystem.entity.Course;
 //import org.springframework.web.bind.annotation.RequestParam;
 import com.example.studentmanagentsystem.entity.Instructor;
 import com.example.studentmanagentsystem.entity.Student;
+import com.example.studentmanagentsystem.entity.repository.CourseRepository;
 import com.example.studentmanagentsystem.entity.repository.InstructorRepository;
 import com.example.studentmanagentsystem.entity.repository.StudentRepository;
 import com.example.studentmanagentsystem.model.LoginModel;
@@ -20,51 +26,57 @@ import jakarta.validation.Valid;
 @Controller
 public class projectControllers {
 
+	@Autowired
+	private StudentRepository studentRepository;
+
+	@Autowired
+	private InstructorRepository instructorRepository;
+	
+	@Autowired
+	private CourseRepository courseRepository;
+	
     @Autowired
-    private StudentRepository studentRepository;
+    private CourseDAO courseDAO;
 
-        @Autowired
-    private InstructorRepository instructorRepository;
+	// Route for home page
+	@GetMapping("/")
+	public String homePage() {
+		return "home";
+	}
 
-        // Route for home page
-    @GetMapping("/")
-    public String homePage() {
-        return "home";
-    }
+	// Route for sign-up page
+	@GetMapping("/signup")
+	public String signupPage(Model model) {
+		model.addAttribute("student", new Student());
+		return "signup";
+	}
 
-    // Route for sign-up page
-    @GetMapping("/signup")
-    public String signupPage(Model model) {
-        model.addAttribute("student", new Student());
-        return "signup";
-    }
+	// Handler for sign-up
+	@PostMapping("/signupprocess")
+	public String signup(@Valid @ModelAttribute("student") Student student, BindingResult bindingResult) {
+		if (bindingResult.hasErrors()) {
+			// Validation failed, return to the sign-up form with error messages
+			return "signup";
+		}
 
-    // Handler for sign-up
-    @PostMapping("/signupprocess")
-    public String signup(@Valid @ModelAttribute("student") Student student, BindingResult bindingResult) {
-        if (bindingResult.hasErrors()) {
-            // Validation failed, return to the sign-up form with error messages
-            return "signup";
-        }
+		try {
+			student.setRole("USER");
+			studentRepository.save(student);
+			return "redirect:/"; // Redirect to the home page after successful signup
+		} catch (DataIntegrityViolationException e) {
+			bindingResult.rejectValue("email", "error.student", "Email already exists");
+			return "signup";
+		}
+	}
 
-        try {
-        	student.setRole("USER");
-            studentRepository.save(student);
-            return "redirect:/"; // Redirect to the home page after successful signup
-        } catch (DataIntegrityViolationException e) {
-            bindingResult.rejectValue("email", "error.student", "Email already exists");
-            return "signup";
-        }
-    }
-    
-    @GetMapping("/login")
-    public String loginPage(Model model) {
-    	model.addAttribute("loginModel", new LoginModel());
-        return "login";
-    }
-    
+	@GetMapping("/login")
+	public String loginPage(Model model) {
+		model.addAttribute("loginModel", new LoginModel());
+		return "login";
+	}
+
 	// handler for login process
-    @PostMapping("/login")
+	@PostMapping("/login")
 	public String login(@ModelAttribute("loginModel") LoginModel loginModel) {
 		if ("student,".equals(loginModel.getRole())) {
 			Student dbUser = studentRepository.findByEmail(loginModel.getEmail());
@@ -74,8 +86,7 @@ public class projectControllers {
 			} else {
 				return "error";
 			}
-		}
-		else {
+		} else {
 			Instructor dbUser = instructorRepository.findByEmail(loginModel.getEmail());
 			if (dbUser != null && loginModel.getPassword().equals(dbUser.getPassword())) {
 				if ("ADMIN".equals(dbUser.getRole())) {
@@ -86,9 +97,18 @@ public class projectControllers {
 			} else {
 				return "error";
 			}
-	
 		}
-		}
-        
 	}
 
+	@GetMapping("/addcourse")
+	public String addCourse(Model model) {
+		model.addAttribute("course", new Course());
+		return "addcourse";
+	}
+	@PostMapping("/addcourseprocess")
+	public String addCourseProcess(Course course) {
+		courseRepository.save(course);
+		return "adminpage";
+	}
+
+}

9c2b20866a5ca8e06a6928bcd8cbc33e83743985
Tejas Raikwar <raikwar.tejas@gmail.com>
2023-10-21 22:50:04 +0530

    -

--- src/main/java/com/example/studentmanagentsystem/controller/projectControllers.java
+++ src/main/java/com/example/studentmanagentsystem/controller/projectControllers.java
@@ -11,9 +11,11 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 import com.example.studentmanagentsystem.entity.Course;
+import com.example.studentmanagentsystem.entity.Enrollment;
 import com.example.studentmanagentsystem.entity.Instructor;
 import com.example.studentmanagentsystem.entity.Student;
 import com.example.studentmanagentsystem.entity.repository.CourseRepository;
+import com.example.studentmanagentsystem.entity.repository.EnrollmentRepository;
 import com.example.studentmanagentsystem.entity.repository.InstructorRepository;
 import com.example.studentmanagentsystem.entity.repository.StudentRepository;
 import com.example.studentmanagentsystem.model.LoginModel;
@@ -32,6 +34,9 @@ public class projectControllers {
 	
 	@Autowired
 	private CourseRepository courseRepository;
+	
+	@Autowired
+	private EnrollmentRepository enrollmentRepository;
 	
     @Autowired
     CourseService courseService;
@@ -114,11 +119,67 @@ public class projectControllers {
 		return "adminpage";
 	}
 	
-	
 	@GetMapping("/allcourses")
 	public ModelAndView getAllBook() {
 		List<Course> list = courseService.getAllCourse();
 		return new ModelAndView("courseslist", "course", list);
+	}
+	
+	
+//    @GetMapping("/instructor")
+//    public ModelAndView instructorSignup(Model model) {
+//		List<Course> list = courseService.getAllCourse();
+//		model.addAttribute("instructor", new Instructor());
+//		return new ModelAndView("instructor-signup", "course", list);
+//    }
+	
+	@GetMapping("/enrollcourse")
+	public ModelAndView enrollCourse(Model model) {
+		List<Course> list = courseService.getAllCourse();
+		return new ModelAndView("enrollcourse", "course", list);
+	}
+	
+//    @PostMapping("/instructorprocess")
+//    public String instructorSignupProcess(@Valid @ModelAttribute("instructor") Instructor instructor, BindingResult bindingResult) {
+//        if (bindingResult.hasErrors()) {
+//            // Validation failed, return to the sign-up form with error messages
+//            return "instructor-signup";
+//        }
+//        try {
+//            instructor.setRole("INSTRUCTOR");
+//            instructorRepository.save(instructor);
+//            return "redirect:/"; // Redirect to the home page after successful signup
+//        } catch (DataIntegrityViolationException e) {
+//            bindingResult.rejectValue("email", "error.student", "Email already exists");
+//            return "instructor-signup";
+//        }
+//    }
+	
+	@PostMapping("/processenrollcourse")
+	public String processEnrollCourse(@Valid @ModelAttribute("enrollment") Enrollment enrollment,BindingResult bindingResult) {
+      if (bindingResult.hasErrors()) {
+      // Validation failed, return to the sign-up form with error messages
+      return "enrollcourse";
+      }
+      try {
+    	  enrollmentRepository.save(enrollment);
+    	  return "redirect:/";
+      }
+      catch(DataIntegrityViolationException e) {
+    	  bindingResult.rejectValue("email", "error.student", "Email already exists");
+        return "enrollcourse";
+      }
+      
+	}
+	
+	@GetMapping("/enrollments")
+	public String enrollments() {
+		return "enrollments";
+	}
+	
+	@GetMapping("/feedbacks")
+	public String feedbacks() {
+		return "feedback";
 	}
 	
 	@GetMapping("/logout")
